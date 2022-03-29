785aa720388d9a55acaf88759d3c775548af4bd2
Yukta Kumar Shrestha <yukta.shrestha5@gmail.com>
2022-03-29 19:28:19 +0545

    dist file name updated

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/SuperMarket"));
+app.use(express.static(__dirname + "/dist/super-market"));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/SuperMarket/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/super-market/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
